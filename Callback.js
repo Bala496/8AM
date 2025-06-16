f173231e1f90313b21301229aca75f5ba0b1cca4
Bala496 <balagurus2002@gmail.com>
2025-06-16 08:29:11 +0530

    Updated

---
+++ Callback.js
@@ -0,0 +1,26 @@
+//callback 
+
+const a = (k) =>{
+    console.log("function1 is executed");
+    k();
+}
+
+function b() {
+    console.log("function2 is executed");
+    
+}
+a(b);
+
+function bookroom(a) {
+
+    setTimeout(() => {
+        console.log("Room is booked");
+        a();
+    }, 3000);
+}
+
+function checkin() {
+    console.log("I am check in the room");
+    
+}
+bookroom(checkin);
